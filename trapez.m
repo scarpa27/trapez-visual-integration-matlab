ecc4f35e2974d586696f61880ce736b64259039c
Toni Čizmić <tcizmic27@gmail.com>
2025-02-06 01:18:54 +0100

    varargs

--- trapez.m
+++ trapez.m
@@ -1,23 +1,33 @@
-function trapez(f_string)
+function trapez(f_string, g_string, varargin)
 
 
+if isempty(f_string)
+    f_string = 'x.^2';
+end
+if isempty(g_string)
+    g_string = '-x.^2 +4';
+end
 
 x_limit_low = 0;
 x_limit_high = 5;
 x_num_steps_min = 2;
 x_num_steps_max = 100;
 
+if length(varargin) == 2
+    x_limit_low = varargin{1};
+    x_limit_high = varargin{2};
+end
+
 
 s = sym('s');
 x_axis = @(x) 0;
 fun = str2func(append('@(x) ', f_string));
-% real_area = integral(@(x) fun(x)-x_axis(x), x_limit_low, x_limit_high);
+gun = str2func(append('@(x) ', g_string));
 
-rafun1 = str2func(append('@(s) ', replace(f_string, 'x', 's') ));
-rafun2 = (@(s) (-(s.^2)+5));
-rafun2 = (@(s) s.^2-1);
+ra_fun = str2func(append('@(s) ', replace(f_string, 'x', 's') ));
+ra_gun = str2func(append('@(s) ', replace(g_string, 'x', 's') ));
 
-real_area = get_real_area(rafun1, rafun2);
+real_area = get_real_area(ra_fun, ra_gun);
 
 log_base=3;
 log_min = log(x_num_steps_min)/log(log_base);
@@ -36,7 +46,7 @@ slider = uicontrol('Style', 'slider',...
     'Min', log_min, 'Max', log_max,...
     'Value', x_num_steps_initial,...
     'Units', 'normalized', 'Position', s_pos);
-slider.Callback = @(source,event) update_plot(fun, ax, source, x_limit_low, x_limit_high, real_area, log_base);
+slider.Callback = @(source,event) update_plot(fun, gun, ax, source, x_limit_low, x_limit_high, real_area, log_base);
 
 % gumbovi + -
 b_width = 0.1;
@@ -46,14 +56,14 @@ uicontrol('Style', 'pushbutton',...
         'String', '-',...
         'Units', 'normalized',...
         'Position', [s_pos(1) - b_width, 0.05, b_width, b_height],...
-        'Callback', @(source,event) adjust_steps(fun, slider, -1, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base));
+        'Callback', @(source,event) adjust_steps(fun, gun, slider, -1, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base));
 uicontrol('Style', 'pushbutton',...
         'String', '+',...
         'Units', 'normalized',...
         'Position', [s_pos(1) + s_width, 0.05, b_width, b_height],...
-        'Callback', @(source,event) adjust_steps(fun, slider, 1, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base));
+        'Callback', @(source,event) adjust_steps(fun, gun, slider, 1, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base));
 
-    function adjust_steps(fun, slider, step_change, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base)
+    function adjust_steps(fun, gun, slider, step_change, x_num_steps_min, x_num_steps_max, ax, x_limit_low, x_limit_high, real_area, log_base)
         log_steps = get(slider, 'Value');
         current_steps = log_base^log_steps;
 
@@ -64,15 +74,15 @@ uicontrol('Style', 'pushbutton',...
 
         log_steps = log(new_steps)/log(log_base);
         set(slider, 'Value', log_steps);
-        update_plot(fun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
+        update_plot(fun, gun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
     end
 
 
 % početna slika
-update_plot(fun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
+update_plot(fun, gun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
 
 
-    function update_plot(fun, ax, source, x_limit_low, x_limit_high, real_area, log_base)
+    function update_plot(fun, gun, ax, source, x_limit_low, x_limit_high, real_area, log_base)
     log_val = get(source,'Value'); % 0 - logbase(maxstep)
     x_num_steps = round(log_base^log_val);
     set(source,'Value', (log(x_num_steps)/log(log_base)));
@@ -80,26 +90,35 @@ update_plot(fun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
 
     x_step_diff = (x_limit_high-x_limit_low)/x_num_steps;
     x = x_limit_low : x_step_diff : x_limit_high;
-    y = fun(x);
+    y = fun(x)-gun(x);
+    yf = fun(x);
+    yg = gun(x);
+
+    [yf, yg] = adj_vct_size(yf, yg);
 
     cla(ax);  % Očisti staru sliku!!
 
     fplot(ax, fun, [x_limit_low, x_limit_high], 'Color', 'b', 'LineWidth', 1); % prava krivulja
     hold(ax, 'on');
+    fplot(ax, gun, [x_limit_low, x_limit_high], 'Color', 'r', 'LineWidth', 1); % prava krivulja
 
-    p = area(x,y, 'Parent', ax);
+    pf = area(x,yf, 'Parent', ax);
+    pg = area(x,yg, 'Parent', ax);
     grid(ax);
 
-    p.FaceColor = "#dba5f2";
-    p.FaceAlpha = 0.2;
-    p.LineWidth = 2;
+    pf.FaceColor = "#dba5f2";
+    pf.FaceAlpha = 0.2;
+    pf.LineWidth = 2;
+    pg.FaceColor = "#dba5f2";
+    pg.FaceAlpha = 0.2;
+    pg.LineWidth = 2;
 
     avg_y = y(1:length(x)-1) + diff(y)/2;
-    A = sum(diff(x) .* avg_y);
+    A = sum(diff(x) .* abs(avg_y));
 
     
     for i = 1:length(x)
-        plot(ax, [x(i), x(i)], [0, y(i)], 'Color', "#122c6e", 'LineWidth', 2);
+        plot(ax, [x(i), x(i)], [min(yf(i)), max(yg(i))], 'Color', "#122c6e", 'LineWidth', 2);
     end
     hold(ax, 'off');
 
@@ -113,7 +132,8 @@ update_plot(fun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
         area = 0;
 
         r = sort(solve(f(s) == g(s), s));
-        r = [x_limit_low, r', x_limit_high];
+        r = [x_limit_low, r', x_limit_high]
+        r = r(imag(r)==0)
         r = r(r >= x_limit_low & r <= x_limit_high);
         r = double(r);
 
@@ -121,6 +141,21 @@ update_plot(fun, ax, slider, x_limit_low, x_limit_high, real_area, log_base);
             a = integral(@(s) abs(f(s) - g(s)), r(i), r(i+1));
             area = area + a;
         end
+    end
+
+    function [v1, v2] = adj_vct_size(a, b)
+        sa = length(a);
+        sb = length(b);
+
+        v1 = a;
+        v2 = b;
+        if (sa < sb && sa == 1) || (sb < sa && sb == 1)
+            if sa < sb  % vector1 is smaller and of size 1
+                v1 = repmat(a, 1, sb);
+            else
+                v2 = repmat(b, 1, sa);
+            end
+         end
     end
 
 
